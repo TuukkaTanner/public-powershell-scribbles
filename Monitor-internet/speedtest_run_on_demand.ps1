18fad4039334b5aa0e52c622b10d5793f237dfbc
TuukkaTanner <74246563+TuukkaTanner@users.noreply.github.com>
2021-11-09 15:18:13 +0200

    Add files via upload

---
+++ Monitor-internet/speedtest_run_on_demand.ps1
@@ -0,0 +1,62 @@
+# Tests internet connection speed via Speedtest CLI tool
+# Only tests the Speed of current network
+# Compares to the latest test if one is available and notes if the speed is within 20% of the last test
+ 
+#Replace the Download URL to where you've uploaded the ZIP file yourself. We will only download this file once. 
+#Latest version can be found at: https://www.speedtest.net/nl/apps/cli
+
+$DownloadURL = "https://bintray.com/ookla/download/download_file?file_path=ookla-speedtest-1.0.0-win64.zip"
+$DownloadLocation = "$($Env:ProgramData)\SpeedtestCLI"
+try {
+    $TestDownloadLocation = Test-Path $DownloadLocation
+    if (!$TestDownloadLocation) {
+        new-item $DownloadLocation -ItemType Directory -force
+        Invoke-WebRequest -Uri $DownloadURL -OutFile "$($DownloadLocation)\speedtest.zip"
+        Expand-Archive "$($DownloadLocation)\speedtest.zip" -DestinationPath $DownloadLocation -Force
+    } 
+}
+catch {  
+    write-host "The download and extraction of SpeedtestCLI failed. Error: $($_.Exception.Message)"
+    exit 1
+}
+$PreviousResults = if (test-path "$($DownloadLocation)\LastResults.txt") { get-content "$($DownloadLocation)\LastResults.txt" | ConvertFrom-Json }
+$SpeedtestResults = & "$($DownloadLocation)\speedtest.exe" --format=json --accept-license --accept-gdpr
+$SpeedtestResults | Out-File "$($DownloadLocation)\LastResults.txt" -Force
+$SpeedtestResults = $SpeedtestResults | ConvertFrom-Json
+ 
+#creating object
+[PSCustomObject]$SpeedtestObj = @{
+    downloadspeed = [math]::Round($SpeedtestResults.download.bandwidth / 1000000 * 8, 2)
+    uploadspeed   = [math]::Round($SpeedtestResults.upload.bandwidth / 1000000 * 8, 2)
+    packetloss    = [math]::Round($SpeedtestResults.packetLoss)
+    isp           = $SpeedtestResults.isp
+    ExternalIP    = $SpeedtestResults.interface.externalIp
+    InternalIP    = $SpeedtestResults.interface.internalIp
+    UsedServer    = $SpeedtestResults.server.host
+    ResultsURL    = $SpeedtestResults.result.url
+    Jitter        = [math]::Round($SpeedtestResults.ping.jitter)
+    Latency       = [math]::Round($SpeedtestResults.ping.latency)
+}
+$SpeedtestHealth = @()
+#Comparing against previous result. Alerting is download or upload differs more than 20%.
+if ($PreviousResults) {
+    if ($PreviousResults.download.bandwidth / $SpeedtestResults.download.bandwidth * 100 -le 80) { $SpeedtestHealth += "Download speed difference is more than 20%" }
+    if ($PreviousResults.upload.bandwidth / $SpeedtestResults.upload.bandwidth * 100 -le 80) { $SpeedtestHealth += "Upload speed difference is more than 20%" }
+}
+if (!$SpeedtestHealth) {
+    $SpeedtestHealth = "Connection speed is within 20% of last results. Below are current test results."
+}
+$SpeedtestHealth
+$SpeedtestResults
+
+if ($PreviousResults) {
+write-host "Below are also the last results (not the above run) of the SpeedTest that was run on the device"
+$PreviousResults
+}
+
+# If running in the console, wait for input before closing.
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp") > $null
+}
